af3eff81414946b0922ee309a3150a4032b337b2
vv <grub@ukr.net>
2018-10-10 15:17:02 +0300

    add models.py

---
+++ app/models.py
@@ -0,0 +1,34 @@
+from sqlalchemy import Table, Column, Integer, Float, String, MetaData, DateTime, ForeignKey, \
+    create_engine
+
+metadata = MetaData()
+
+contracts = Table('contracts', metadata,
+                  Column('id', Integer, primary_key=True, autoincrement=True),
+                  Column('title', String, nullable=False),
+                  Column('price', Float),
+                  Column('comment', String),
+                  Column('expiration_date', DateTime)
+                  )
+
+payments = Table('payments', metadata,
+                 Column('id', Integer, primary_key=True, autoincrement=True),
+                 Column('contracts_id', None, ForeignKey('contracts.id')),
+                 Column('amount', Float),
+                 Column('date', DateTime)
+                 )
+
+
+# engine = create_engine('sqlite:///:memory:', echo=True)
+engine = create_engine('postgresql://postgres:postgres@localhost:5432/postgres', echo=True)
+
+conn = engine.connect()
+metadata.create_all(engine)
+contracts.columns.items()
+
+
+ins = contracts.insert().values(title='A1', price='100.00', comment='Contract 1')
+result = conn.execute(ins)
+# result.inserted_primary_key
+
+print(str(ins))
