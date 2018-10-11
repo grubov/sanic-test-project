eb6252b15c4d9d64a9b9f0d228b89277186d382f
vv <grub@ukr.net>
2018-10-11 13:03:05 +0300

    add simple post

--- app/app.py
+++ app/app.py
@@ -7,17 +7,38 @@ app = Sanic()
 
 @app.route('/', methods=['GET'])
 async def get_contracts(request):
+    s = select([contracts])
+    conn = engine.connect()
+    result = conn.execute(s)
+    return response.json({result})
+
+#
+# @app.route('/', methods=['POST'])
+# async def post_contract(request):
+#     ins = select([contracts])
+#     conn = engine.connect()
+#     result = conn.execute(ins)
+#     return response.json({"POST": request.json})
+
+
+@app.route('/', methods=['POST'])
+async def post_contract(request):
     ins = select([contracts])
     conn = engine.connect()
+    title = request.json.get('title')
+    price = request.json.get('price')
+    comment = request.json.get('comment')
+    user =
+
     result = conn.execute(ins)
-    return response.json({result})
+    return response.json({"POST": title})
 
 
 @app.route('/<contract_id:int>', methods=['GET'])
 async def get_contract(request, contract_id):
-    ins = select([contracts]).where(contracts.c.id == contract_id)
+    s = select([contracts]).where(contracts.c.id == contract_id)
     conn = engine.connect()
-    result = conn.execute(ins)
+    result = conn.execute(s)
     return response.json({result})
 
 
