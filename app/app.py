8dfa48183564f24e2f0572189264fe5cb6926d97
vv <grub@ukr.net>
2018-10-11 16:15:21 +0300

    add contract post

--- app/app.py
+++ app/app.py
@@ -12,34 +12,26 @@ async def get_contracts(request):
     result = conn.execute(s)
     return response.json({result})
 
-#
-# @app.route('/', methods=['POST'])
-# async def post_contract(request):
-#     ins = select([contracts])
-#     conn = engine.connect()
-#     result = conn.execute(ins)
-#     return response.json({"POST": request.json})
-
 
 @app.route('/', methods=['POST'])
 async def post_contract(request):
     conn = engine.connect()
-    title = request.json.get('title')
-    price = request.json.get('price')
-    comment = request.json.get('comment')
-    ins = contracts.insert()
 
-    # stmt = contracts.__table__.insert()
-    cont = {
-        'title': title,
-        'price': price,
-        'comment': comment
-    }
-    # conn.execute(stmt, cont)
+    # title = request.json.get('title')
+    # price = request.json.get('price')
+    # comment = request.json.get('comment')
+
+    ins = contracts.insert()
+    cont = request.json
+
+    # cont = {
+    #     'title': title,
+    #     'price': price,
+    #     'comment': comment
+    # }
 
     result = conn.execute(ins, cont)
-
-    return response.json({"POST": "OK"})
+    return response.json({'POST': f'CONTRACT ADDED TO DATABASE WITH ID={result.inserted_primary_key}'})
 
 
 @app.route('/<contract_id:int>', methods=['GET'])
@@ -51,4 +43,4 @@ async def get_contract(request, contract_id):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000, debug=True)
