18bcd5d4ffe6301b412873498216bdc3b9c51c39
vv <grub@ukr.net>
2018-10-11 18:27:25 +0300

    add contract put

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from sanic import Sanic
 from sanic import response
+from sqlalchemy import update
 from sqlalchemy.sql import select
 from models import contracts, payments, engine
 app = Sanic()
@@ -22,7 +23,7 @@ async def post_contract(request):
     # comment = request.json.get('comment')
 
     ins = contracts.insert()
-    cont = request.json
+    json_data = request.json
 
     # cont = {
     #     'title': title,
@@ -30,7 +31,7 @@ async def post_contract(request):
     #     'comment': comment
     # }
 
-    result = conn.execute(ins, cont)
+    result = conn.execute(ins, json_data)
     return response.json({'POST': f'CONTRACT ADDED TO DATABASE WITH ID={result.inserted_primary_key}'})
 
 
@@ -40,6 +41,15 @@ async def get_contract(request, contract_id):
     conn = engine.connect()
     result = conn.execute(s)
     return response.json({result})
+
+
+@app.route('/<contract_id:int>', methods=['PUT'])
+async def put_contract(request, contract_id):
+    json_data = request.json
+    u = contracts.update().where(contracts.c.id == contract_id)
+    conn = engine.connect()
+    result = conn.execute(u, json_data)
+    return response.json({'POST': f'CONTRACT UPDATED IN DATABASE WITH ID={contract_id}'})
 
 
 if __name__ == "__main__":
