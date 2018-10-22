dea3dfa0958f627e9ae2a8de7d8f4d8541536901
vv <grub@ukr.net>
2018-10-22 17:13:26 +0300

    add pytest for some routes

---
+++ tests/test.py
@@ -0,0 +1,33 @@
+from service_api.app import app
+
+def test_smoke():
+    """Start with a blank database."""
+    request, response = app.test_client.get('/contracts/v1/smoke')
+    assert response.status == 200
+
+
+def test_contracts_returns_200():
+    request, response = app.test_client.get('/contracts/v1/contracts/')
+    assert response.status == 200
+
+
+def test_not_found_error():
+    request, response = app.test_client.get('/')
+    assert response.status == 404
+
+
+def test_get_contract():
+    data = {
+        "id": 7,
+        "title": "A1",
+        "price": 100,
+        "comment": "Contract 117",
+        "expiration_date": 1516233600
+    }
+    request, response = app.test_client.get('/contracts/v1/contracts/7')
+    assert response.json == data
+
+
+def test_get_id_of_contract():
+    request, response = app.test_client.get('/contracts/v1/contracts/6')
+    assert response.json.get('id') == 6
