269eae1b5bf1b206493afb6deb9b9567acc2dced
vv <grub@ukr.net>
2018-10-30 18:14:15 +0200

    add argparse subparser (manage.py dbinit)

--- domain/contracts.py
+++ domain/contracts.py
@@ -1,14 +1,22 @@
 from sqlalchemy.sql import select
 
 from service_api.models import Contracts
-from service_api.database import engine
+# from service_api.database import engine
+from aiopg.sa import create_engine
+
+dsn = 'dbname=postgres user=postgres password=postgres host=127.0.0.1'
 
 
 async def get_contract_by_id(contract_id):
-    stmt = select([Contracts]).where(Contracts.c.id == contract_id)
-    conn = engine.connect()
-    result = conn.execute(stmt)
-    return dict(result.fetchone())
+    async with create_engine(dsn) as engine:
+        async with engine.acquire() as conn:
+            stmt = select([Contracts]).where(Contracts.c.id == contract_id)
+            result = await conn.execute(stmt)
+            d = {}
+            # for rowproxy in result:
+            #     d = dict(rowproxy.items())
+            a = [i.items() for i in result]
+            return a
 
 
 async def put_contract_by_id(request, contract_id):
